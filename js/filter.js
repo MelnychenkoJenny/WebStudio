7112ab09f5ea69c9bd67a4ac15bb6b79e5e0c5a0
MelnychenkoJenny <melnichenkojenny@gmail.com>
2023-06-05 23:01:13 +0300

    add filter (JS) in portfolio page

---
+++ js/filter.js
@@ -0,0 +1,17 @@
+const buttonsFilter = document.querySelectorAll('.filtr-item button');
+const cardsItem = document.querySelectorAll('.projects-item');
+
+console.log(projectsArray)
+buttonsFilter.forEach(button => {
+  button.addEventListener('click', () => {
+    const category = button.dataset.category;
+    
+    cardsItem.forEach(card => {
+      const project = card.dataset.category;
+      if (category === 'all' || project === category) {
+        return (card.style.display = 'block');
+      }
+      return (card.style.display = 'none');
+    });
+  });
+});
